9ec7563494ef0dff254c6c5d5219e05b14e7c0e3
ERE <ernesto_711@live.com.mx>
2022-06-24 23:21:34 -0500

    finished v 1.1

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
 const optBtns = document.querySelectorAll(".optBtn");
-//const hijo = optBtns[0].firstChild;
+const btnSubmit = document.querySelector(".rating__optRating--submitBtn");
+let ratingSelected = "0";
+
+const optRatingScreen = document.querySelector(".ratingCard__optRating");
+const thanksScreen = document.querySelector(".ratingCard__msjGratitude");
+const msjUserFeddBack = document.querySelector(".ratingCard__msjGratitude--userFeddBack");
+
 
 for (let i = 0; i < optBtns.length; i++) {
     const element = optBtns[i];
     
     element.addEventListener("click", (e)=>{
-        console.log(e.target.firstChild.textContent);
-
-        e.target.parentNode.classList.add("selected");
-
-
+        if(e.target.classList.contains("optBtn")){
+            for (let j = 0; j < optBtns.length; j++) {
+                const nodo = optBtns[j];
+                nodo.classList.contains("selected")&& nodo.classList.remove("selected");
+            }
+            e.target.classList.add("selected");
+            ratingSelected = e.target.textContent;
+        }else{ 
+            for (let j = 0; j < optBtns.length; j++) {
+                const nodo = optBtns[j];
+                nodo.classList.contains("selected")&& nodo.classList.remove("selected");
+            }
+            e.target.parentNode.classList.add("selected");  
+            ratingSelected = e.target.textContent;         
+        }
     })
-
-
-
-//    console.log(element.firstChild.textContent);
 }
+
+
+btnSubmit.addEventListener("click", function () {
+        optRatingScreen.classList.add("deactivate");
+        thanksScreen.classList.add("active");
+        msjUserFeddBack.firstChild.textContent = `You selected ${ratingSelected} out of 5`;
+    });
